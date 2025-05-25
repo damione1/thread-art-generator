7a5a6b433468a4b57ebaa25bbe93adcd9a33209b
Damien Goehrig <14912510+damione1@users.noreply.github.com>
2025-05-25 16:40:24 -0400

    remove next client

--- web/src/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-// Form components
-export * from './FormField';
-export * from './Input';
-export * from './FormMessage';
-
-// Re-export the useForm hook for convenience
-export { useForm } from '../../hooks/useForm';
