74a1d20798b1ada70c2fa67c07dd83d5eaba818c
Damien Goehrig <14912510+damione1@users.noreply.github.com>
2025-05-25 16:40:24 -0400

    remove next client

--- web/src/middleware.ts
+++
@@ -1,6 +0,0 @@
-// We don't need server-side middleware for Auth0 SPA authentication
-// This file is left empty but can be used in the future if needed
-
-export const config = {
-    matcher: [], // Don't match any routes
-};
